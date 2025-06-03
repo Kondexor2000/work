c3930d3aff5279dd07b1130e7d7af80f45f30176
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2025-06-03 22:20:08 +0200

    Użyj DATABASE_URL z Heroku

--- work/settings.py
+++ work/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,14 +79,7 @@ WSGI_APPLICATION = 'work.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'work',  # Replace with your database name
-        'USER': 'postgres',  # Replace with your database user
-        'PASSWORD': 'postgres',  # Replace with your database password
-        'HOST': 'localhost',  # Or your database server's address
-        'PORT': '5432',  # Default PostgreSQL port
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
