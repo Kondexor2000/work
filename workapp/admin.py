7281c1943d7a4cd48d43a62d6f4cec9bbc944f3f
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2025-06-06 08:58:19 +0200

    Add Admin

--- workapp/admin.py
+++ workapp/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class CategoryEmployAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(CategoryEmploy, CategoryEmployAdmin)
+
+class CategoryCourseAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(CategoryCourse, CategoryCourseAdmin)
+
+class TagCourseAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(TagCourse, TagCourseAdmin)
+
+class TagPortfolioAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(TagPortfolio, TagPortfolioAdmin)
+
+class TagBusinessAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(TagBusiness, TagBusinessAdmin)
