f20845c34b72983ee1c672866bff8427baf65219
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2025-07-11 10:46:52 +0200

    Security v5

--- workapp/signals.py
+++ workapp/signals.py
@@ -2,7 +2,16 @@ from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 from .models import LoginLog
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        # może zawierać listę IP oddzielonych przecinkami
+        ip = x_forwarded_for.split(',')[0].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 @receiver(user_logged_in)
 def log_user_login(sender, request, user, **kwargs):
-    ip = request.META.get('REMOTE_ADDR')
+    ip = get_client_ip(request)
     LoginLog.objects.create(user=user, ip_address=ip)
