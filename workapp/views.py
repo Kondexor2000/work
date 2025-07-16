6e0c3f3046cc26e3b6f1d62eee19a564e606f141
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2025-07-16 23:29:45 +0200

    Update front-end v50

--- workapp/views.py
+++ workapp/views.py
@@ -579,7 +579,7 @@ class AddAnswersView(LoginRequiredMixin, CreateView):
     
 class AddQuestionnaireView(LoginRequiredMixin, CreateView):
     form_class = QuestionnaireForm
-    template_name = 'add_questionnaire.html'
+    template_name = 'questionnaire_list.html'
 
     def form_valid(self, form):
         business_id = self.kwargs.get('course_id')
